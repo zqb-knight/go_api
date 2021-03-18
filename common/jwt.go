9bb1fb12356e20fe224e189da537e79415b7880c
zqb-knight <42669984+zqb-knight@users.noreply.github.com>
2021-03-18 17:46:20 +0800

    Create jwt.go
    添加jwt验证函数

---
+++ common/jwt.go
@@ -0,0 +1,45 @@
+package jwt
+
+import (
+	jwt "github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+var screte string = "0115"
+
+type Claims struct{
+	UserName string `json:"username"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(userName string) (string, error){
+	nowTime := time.Now()
+	expireTime := nowTime.Add(4 * time.Hour)
+
+	claims := Claims{
+		userName,
+		jwt.StandardClaims {
+			ExpiresAt: expireTime.Unix(),
+			Issuer: "zqb",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(screte)
+
+	return ss, err
+
+}
+
+func ParseToken(tokenString string) (*Claims, error){
+	toekn, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token)(interface{}, error){
+		return screte, nil
+	})
+	if toekn != nil{
+		if cliams, ok := toekn.Claims.(*Claims); ok && toekn.Valid{
+			return cliams, nil
+		}
+	}
+
+	return nil,err
+
+}
